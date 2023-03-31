6f1d3364f341236f07e09571b6a43080c414754d
roman <ronnoverro@gmail.com>
2023-03-31 13:54:34 -0700

    ascii table in progress

--- HOMEWORK/asciiTable/src/Ascii.java
+++ HOMEWORK/asciiTable/src/Ascii.java
@@ -5,31 +5,53 @@ public class Ascii {
     {
         int count;
 
+        
         System.out.println("This program will print out all the ascii characters 33-126");
         System.out.println("using three different types of loops.");
         System.out.println();
 
-        // with a for loop:
-        System.out.println("This is a countdown using a for loop.");
-        for (int i = 33; i <= 126; i++)
-        {
-            System.out.println(i + " = " + (char)i);
-        }
+        
+        System.out.println("do you want to print with for loop, while loop, or do-loop?");
 
-        // with a while loop:
-        count = 33;
-        while (count <= 126)
-        {
-            System.out.println(count + " = " + (char)count);
-            count++;
-        }
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String option = myObj.next();  // Read user input
 
-        // with a do-while loop:
-        count = 33;
-        do
-        {
-            System.out.println(count + " = " + (char)count); 
-            count++;
-        } while (count <= 126);
+        switch (option) {
+            // CHECK IF 'I' IS A MULTIPLE OF 8 (NUM. OF CHARS PER LINE). IF SO, NEW LINE.
+            case "for":
+                // with a for loop:
+                System.out.println("This is a countdown using a for loop.");
+                for (int i = 33; i <= 126; i++)
+                {
+                    System.out.printf("%5s", i + " = " + (char)i + " ");
+                }
+                break;
+
+
+
+            case "while":
+                // with a while loop:
+                count = 33;
+                System.out.println("This is a countdown using a while loop.");
+                while (count <= 126)
+                {
+                    System.out.println(count + " = " + (char)count);
+                    count++;
+                }
+                break;
+            case "do-while":
+                // with a do-while loop:
+                count = 33;
+                System.out.println("This is a countdown using a do-while loop.");
+                do
+                {
+                    System.out.println(count + " = " + (char)count); 
+                    count++;
+                } while (count <= 126);
+                break;
+            default: 
+                System.out.println("That is not an option");
+        }
+        myObj.close();
     }
 }
