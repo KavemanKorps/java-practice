8b15259bfecafa91e26d5cb142306866012f90ca
roman <ronnoverro@gmail.com>
2023-04-04 09:25:37 -0700

    ascii table complete

--- HOMEWORK/asciiTable/src/Ascii.java
+++ HOMEWORK/asciiTable/src/Ascii.java
@@ -15,7 +15,8 @@ public class Ascii {
 
         do
         {
-            System.out.println("do you want to print with a for loop, while loop, do-loop,");
+            System.out.println("");
+            System.out.println("do you want to print with a for, while, do-while loop,");
             System.out.println("or do you want to exit the program?");
 
             option = myObj.next();  // Read user input
@@ -24,17 +25,21 @@ public class Ascii {
                 // CHECK IF 'I' IS A MULTIPLE OF 8 (NUM. OF CHARS PER LINE). IF SO, NEW LINE.
                 case "exit":
                     System.out.println("exiting program...");
-                    break;
+                    System.out.println();
+                    // break;
+                    System.exit(0);
     
                 case "for":
                     // with a for loop:
                     System.out.println("This is a countdown using a for loop.");
-                    for (int i = 33; i <= 126; i++)
+                    for (count = 33; count <= 126; count++)
                     {
-                        System.out.format("%3d = %-2c", i, (char)(i));
-                        if (i % 8 == 0)
+                        System.out.format("%3d = %-2c", count, (char)(count));
+                        if (count % 8 == 0)
                             System.out.println();
                     }
+                    System.out.println();
+                    System.out.println();
                     break;
     
                 case "while":
@@ -51,6 +56,8 @@ public class Ascii {
                             System.out.println();
                         count++;
                     }
+                    System.out.println();
+                    System.out.println();
                     break;
 
                 case "do-while":
@@ -59,21 +66,20 @@ public class Ascii {
                     System.out.println("This is a countdown using a do-while loop.");
                     do
                     {
-                        // System.out.println(count + " = " + (char)count); 
-                        // count++;
-    
                         System.out.format("%3d = %-2c", count, (char)(count));
                         if (count % 8 == 0)
                             System.out.println();
                         count++;
     
                     } while (count <= 126);
+                    System.out.println();
+                    System.out.println();
                     break;
 
                 default: 
                     System.out.println("That is not an option");
             }
-            
+                   
         }
         while (option != "exit");
 
